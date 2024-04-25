79d1c1e5c2db1557c0324fff6b834e0045968b73
awais5512 <awais.abbas@emumba.com>
2024-04-25 11:08:56 +0500

    Update modal interface styles

--- content-script.js
+++ content-script.js
@@ -1,42 +1,47 @@
 const populateDataContainer = (data) => {
-  const modalContainer = document.createElement('div');
-  const modalTitle = document.createElement('h2');
-  const closeModalButton = document.createElement('button');
+	const modalContainer = document.createElement('div');
+	const modalTitle = document.createElement('h1');
+	const closeModalButton = document.createElement('button');
 
-  modalTitle.textContent = 'Your Favorite Restaurants';
-  modalContainer.appendChild(modalTitle);
+	modalTitle.textContent = 'Food for thought! (and code)';
+	modalContainer.appendChild(modalTitle);
 
-  closeModalButton.classList.add('close-modal-button');
-  closeModalButton.textContent = 'Close Modal';
+	closeModalButton.classList.add('close-modal-button');
+	closeModalButton.textContent = 'Close Modal';
 
-  modalContainer.style.display = 'none';
-  modalContainer.classList.add('modal-container');
+	modalContainer.classList.add('modal-container');
 
-  data.forEach(post => {
-    const p = document.createElement('p');
-    p.classList.add('restaurant-name');
-    p.textContent = post.attributes.Name;
-    p.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+	data.forEach(post => {
+		const p = document.createElement('p');
 
-    modalContainer.appendChild(p);
-    modalContainer.style.display = 'block';
-  })
+		p.classList.add('restaurant-name');
+		p.textContent = post.attributes.Name;
+		p.style.backgroundColor = `rgb(
+			${Math.floor(Math.random() * 255)},
+			${Math.floor(Math.random() * 255)},
+			${Math.floor(Math.random() * 255)}
+		)`;
 
-  closeModalButton.onclick = function() {
-    modalContainer.style.display = 'none';
-  }
+		modalContainer.appendChild(p);
+	})
 
-  modalContainer.appendChild(closeModalButton);
+	modalContainer.style.display = 'block';
 
-  document.body.appendChild(modalContainer);
+	closeModalButton.onclick = function() {
+		modalContainer.style.display = 'none';
+	}
+
+	modalContainer.appendChild(closeModalButton);
+
+	document.body.appendChild(modalContainer);
 }
 
 (() => {
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'fetchedRestaurantsSuccess') {
-      populateDataContainer(message.data);
-    }
-  })
-  
-  chrome.runtime.sendMessage({ action: 'fetchRestaurants' });
+	chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+		if (message.action === 'fetchedRestaurantsSuccess') {
+			populateDataContainer(message.data);
+		}
+	})
+	
+	chrome.runtime.sendMessage({ action: 'fetchRestaurants' });
 })();
