de1f4d149fe99fdd8627c66a40325f7003f7f52f
awais5512 <awais.abbas@emumba.com>
2024-04-25 12:44:10 +0500

    Add support for create restaurant endpoint in modal

--- background.js
+++ background.js
@@ -12,4 +12,28 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       })
       .catch(error => console.error('Error fetching data:', error));
   }
+
+  if (message.action === 'addRestaurant') {
+    fetch('http://localhost:1337/api/restaurants', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+        data: {
+          Name: message.restaurantName
+        }
+      })
+		})
+    .then(response => response.json())
+    .then(data => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          action: "addRestaurantSuccess",
+          data: data
+        })
+      });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+  }
 });
--- content-script.js
+++ content-script.js
@@ -2,12 +2,19 @@ const populateDataContainer = (data) => {
 	const modalContainer = document.createElement('div');
 	const modalTitle = document.createElement('h1');
 	const closeModalButton = document.createElement('button');
+	const restaurantInput = document.createElement('input');
+	const addRestaurantButton = document.createElement('button');
 
 	modalTitle.textContent = 'Food for thought! (and code)';
 	modalContainer.appendChild(modalTitle);
 
 	closeModalButton.classList.add('close-modal-button');
 	closeModalButton.textContent = 'Close Modal';
+
+	restaurantInput.placeholder = 'Enter restaurant name';
+	restaurantInput.classList.add('add-restaurant-field');
+	addRestaurantButton.textContent = 'Add Restaurant';
+	addRestaurantButton.classList.add('add-restaurant-button');
 
 	modalContainer.classList.add('modal-container');
 
@@ -25,21 +32,52 @@ const populateDataContainer = (data) => {
 		modalContainer.appendChild(p);
 	})
 
+	modalContainer.appendChild(restaurantInput);
+	modalContainer.appendChild(addRestaurantButton);
+
 	modalContainer.style.display = 'block';
 
 	closeModalButton.onclick = function() {
 		modalContainer.style.display = 'none';
 	}
 
+	addRestaurantButton.onclick = async function() {
+		const restaurantName = restaurantInput.value;
+		
+		if (!restaurantName) {
+			return;
+		}
+
+		chrome.runtime.sendMessage({ action: 'addRestaurant', restaurantName });
+	};
+
 	modalContainer.appendChild(closeModalButton);
 
 	document.body.appendChild(modalContainer);
+}
+
+const showSuccessAlert = (message) => {
+	const modalContainer = document.querySelector('.modal-container');
+	const alert = document.createElement('div');
+
+	alert.classList.add('alert-success');
+	alert.textContent = message;
+
+	modalContainer.appendChild(alert);
+
+	setTimeout(() => {
+		alert.remove();
+	}, 3000);
 }
 
 (() => {
 	chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 		if (message.action === 'fetchedRestaurantsSuccess') {
 			populateDataContainer(message.data);
+		}
+
+		if (message.action === 'addRestaurantSuccess') {
+			showSuccessAlert("Restaurant added successfully!");
 		}
 	})
 	
